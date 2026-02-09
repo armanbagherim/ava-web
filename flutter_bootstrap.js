b4099653de1b0c01435f38d3f95138159a31c668
Arman <user@users-MacBook-Air-435.local>
2026-02-09 17:16:15 +0330

    Fix video call issues

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3280676903"
+    serviceWorkerVersion: "3352898778"
   }
 });
