e85594be1e02a94f1bc0be03b69d32eb678e4b02
Arman <user@users-MacBook-Air-435.local>
2026-02-10 14:13:05 +0330

    Fix read issue

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3276124569"
+    serviceWorkerVersion: "860591946"
   }
 });
