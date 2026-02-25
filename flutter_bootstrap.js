ad2e9abc036ffff09006c82eb15604de432379b9
Arman <user@users-MacBook-Air-435.local>
2026-02-25 15:38:14 +0330

    Fix favicon

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "887446861"
+    serviceWorkerVersion: "1105841038"
   }
 });
